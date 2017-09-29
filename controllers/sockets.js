767315c250b670364bf85e671c8a5e3505cd6b95
JamieRez <jamierezendes@gmail.com>
2017-09-29 08:03:30 -0700

    socket-io testing

---
+++ controllers/sockets.js
@@ -0,0 +1,12 @@
+module.exports = function(io){
+    // io.on('connection' , function(socket){
+    //
+    //
+    //     socket.on('btnClick' , function(data){
+    //         console.log("Clicked");
+    //         socket.emit('btnClick');
+    //     });
+    //
+    // 
+    // });
+}
