2445e5de4a567b76a2d490e1b6901f381964d752
James Rezendes <James@MacBook-Pro.local>
2017-09-30 17:16:55 -0700

    Changed email to username

--- controllers/passport.js
+++ controllers/passport.js
@@ -35,11 +35,11 @@ module.exports = function(passport) {
 
     passport.use('local-signup', new LocalStrategy({
         // by default, local strategy uses username and password, we will override with email
-        usernameField : 'email',
+        usernameField : 'username',
         passwordField : 'password',
         passReqToCallback : true // allows us to pass back the entire request to the callback
     },
-    function(req, email, password, done) {
+    function(req, username, password, done) {
 
         // asynchronous
         // User.findOne wont fire unless data is sent back
@@ -47,7 +47,7 @@ module.exports = function(passport) {
 
         // find a user whose email is the same as the forms email
         // we are checking to see if the user trying to login already exists
-        User.findOne({ 'email' :  email }, function(err, user) {
+        User.findOne({ 'username' :  username }, function(err, user) {
             // if there are any errors, return the error
             if (err)
                 return done(err);
@@ -62,7 +62,7 @@ module.exports = function(passport) {
                 var newUser = new User();
 
                 // set the user's local credentials
-                newUser.email    = email;
+                newUser.username   = username;
                 newUser.password = newUser.generateHash(password);
 
                 // save the user
@@ -87,15 +87,15 @@ module.exports = function(passport) {
 
     passport.use('local-login', new LocalStrategy({
         // by default, local strategy uses username and password, we will override with email
-        usernameField : 'email',
+        usernameField : 'username',
         passwordField : 'password',
         passReqToCallback : true // allows us to pass back the entire request to the callback
     },
-    function(req, email, password, done) { // callback with email and password from our form
+    function(req, username, password, done) { // callback with email and password from our form
 
         // find a user whose email is the same as the forms email
         // we are checking to see if the user trying to login already exists
-        User.findOne({ 'email' :  email }, function(err, user) {
+        User.findOne({ 'username' :  username }, function(err, user) {
             // if there are any errors, return the error before anything else
             if (err)
                 return done(err);
---
+++ models/square.js
@@ -0,0 +1,5 @@
+var mongoose = require('mongoose');
+
+var squareSchema = mongoose.Schema({
+
+})
