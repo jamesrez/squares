ce87a5c97c3df8e2a16d661b586e750c0db482dc
JamieRez <jamierezendes@gmail.com>
2017-10-04 15:47:19 -0700

    Added colors and resize for anon squares

--- views/scripts/index.js
+++ views/scripts/index.js
@@ -1,19 +1,26 @@
 var socket = io();
 
-function makeDraggable(){
-    $('.square').draggable({
-        stack : ".square",
-        drag : function(event, ui){
-            var squarePos = $(this).offset();
-            var squareId = $(this).attr('id');
-            socket.emit('updateSquarePos' , {pos : squarePos, id : squareId});
-        }
-    });
+function colorUpdate(jscolor){
+  $(jscolor.styleElement).parent().css('background-color' , '#'+jscolor);
+}
+
+//Make the squares draggable
+function makeDraggable(squareClass){
+    if((squareClass == ".sq-" + $('#userProf').text()) || squareClass == ".squareAnon"){
+        $(squareClass).draggable({
+            stack : ".square",
+            drag : function(event, ui){
+                var squarePos = $(this).offset();
+                var squareId = $(this).attr('id');
+                socket.emit('updateSquarePos' , {pos : squarePos, id : squareId});
+            }
+        });
+        $(squareClass).resizable();
+    }
 }
 
 function getUserSqCount(userEmail){
     var className = ".sq-"+userEmail;
-    console.log($(className).length);
     if(!$(className)){
         return 0;
     }else{
@@ -24,24 +31,41 @@ function getUserSqCount(userEmail){
 
 $(document).ready(function(){
 
-    makeDraggable();
+    makeDraggable('.squareAnon');
 
-
+    //When a square is moved on another client
     socket.on('updateSquarePos' , function(data){
+        //Update the square's offset position
         $('#'+data.id).offset(data.pos);
     });
 
+    //When you create a new square
     $('#newSquareBtn').click(function(){
-        socket.emit('newSquare', {user : $('#userProf').val()});
+        //Send username to server
+        socket.emit('newSquare', {user : $('#userProf').text()});
     });
-
+    //When a client makes a new square
     socket.on('newSquare' , function(data){
+        //Create a unique square id and create a new square
         var userSqCount = getUserSqCount(data.user);
+        var newClass = "sq-"+data.user;
         var newId = "sq-" + data.user + "-" + userSqCount;
-        $('#square1').clone().attr('id' , newId).addClass("sq-"+data.user).appendTo('.squareContainer');
-        makeDraggable();
+        var newSquare = $('.squarePrototype').clone().attr('id' , newId).addClass(newClass).addClass('square').css('display','flex').appendTo('.squareContainer');
+        newSquare.removeClass('squarePrototype')
+        makeDraggable('.'+ newClass);
     })
 
+    //Color When Press C
+    $(document).on("keydown", function(e){
+      if(e.which == 67){
+        $('.color').css('display' , 'block');
+      }
+    });
+    $(document).on("keyup", function(e){
+      if(e.which == 67){
+        $('.color').css('display' , 'none');
+      }
+    });
 
     //USER AUTHENITCATION
     $('#register').click(function(){
