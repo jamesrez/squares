341a003d18bfdd4bdb3da3a232c3d002aedf4cb6
JamieRez <jamierezendes@gmail.com>
2017-10-05 17:12:59 -0700

    Can Now edit square's text

--- views/scripts/index.js
+++ views/scripts/index.js
@@ -21,6 +21,8 @@ function makeDraggable(squareClass){
         });
         $(squareClass).resizable({
           resize : function(event,ui){
+            $(this).css('min-height' , "1px");
+            //$(this).css('min-width' , "1px");
             var squareWidth = $(this).width();
             var squareHeight = $(this).height();
             var squareId = $(this).attr('id');
@@ -42,7 +44,7 @@ function getUserSqCount(userEmail){
 
 
 $(document).ready(function(){
-
+    var userTyping = false;
     makeDraggable('.squareAnon');
 
     //When a square is moved on another client
@@ -62,6 +64,11 @@ $(document).ready(function(){
         $(data.id).css('background-color' , data.color);
     });
 
+    //When a square is retyped on another client
+    socket.on('updateSquareText' , function(data){
+        $(data.squareId).children('.squareText').text(data.text);
+    });
+
     //When you create a new square
     $('#newSquareBtn').click(function(){
         //Send username to server
@@ -73,14 +80,15 @@ $(document).ready(function(){
         var userSqCount = getUserSqCount(data.user);
         var newClass = "sq-"+data.user;
         var newId = "sq-" + data.user + "-" + userSqCount;
-        var newSquare = $('.squarePrototype').clone(true,true).attr('id' , newId).addClass(newClass).addClass('square').css('display','flex').appendTo('.squareContainer');
+        var newSquare = $('.squarePrototype').clone(true);
+        newSquare.attr('id' , newId).addClass(newClass).addClass('square').css('display','flex').appendTo('.squareContainer');
         newSquare.removeClass('squarePrototype');
         makeDraggable('.'+ newClass);
     })
 
     //Color When Press C
     $(document).on("keydown", function(e){
-      if(e.which == 67){
+      if(e.which == 67 && userTyping == false){
         var squareClass = ".sq-" + $('#userProf').text();
         $(squareClass).children('.color').css('display' , 'block');
         $('.squareAnon').children('.color').css('display' , 'block');
@@ -93,6 +101,30 @@ $(document).ready(function(){
         $('.squareAnon').children('.color').css('display' , 'none');
       }
     });
+
+    //Typing when DoubleClick
+    $(document).on("dblclick" , ".square" , function(){
+      if($(this).hasClass("sq-" + $('#userProf').text()) || $(this).hasClass("squareAnon")){
+        var squareText = $(this).children('p');
+        var squareTextEdit = $(this).children('textarea').css('display' , 'block').val(squareText.text());
+        squareText.css('display' , 'none');
+        squareTextEdit.focus();
+        userTyping = true;
+      }else{
+        console.log("test");
+      }
+    });
+    $('.squareTextEdit').blur(function(){
+      userTyping = false;
+      var squareTextEditValue = $(this).val();
+      var squareText = $(this).parent().children('p');
+      squareText.css('display' , 'inline-block').text(squareTextEditValue);
+      $(this).css('display' , 'none');
+      var squareId = "#" + squareText.parent().attr('id');
+      socket.emit('updateSquareText' , {text : squareTextEditValue, squareId : squareId});
+    });
+    autosize($('textarea'));
+
 
     //USER AUTHENITCATION
     $('#register').click(function(){
